0d344912db0c70c53993f3f6dad3a6d6c0406bb2
Laura <laurazenc@gmail.com>
2018-10-07 20:11:28 +0200

    User model

---
+++ packages/server/src/models/User.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+import { hashSync, compareSync } from "bcrypt-nodejs";
+import jwt from "jsonwebtoken";
+import config from "./../config";
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, unique: true },
+    firstName: String,
+    lastName: String,
+    password: String
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", function(next) {
+  if (this.isModified("password")) {
+    this.password = this._hashPassword(this.password);
+    return next();
+  }
+  return next();
+});
+
+UserSchema.methods = {
+  _hashPassword(password) {
+    return hashSync(password);
+  },
+  authenticateUser(password) {
+    return compareSync(password, this.password);
+  },
+  createToken() {
+    return jwt.sign(
+      {
+        _id: this._id
+      },
+      process.env.JWT_SECRET || config.JWT_SECRET
+    );
+  }
+};
+
+export default mongoose.model("User", UserSchema);
