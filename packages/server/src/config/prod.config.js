9c66a4acb6219bc8881838a62ce49dafe74a1c0d
Laura <laurazenc@gmail.com>
2018-10-10 20:19:58 +0200

    Session key

--- packages/server/src/config/prod.config.js
+++ packages/server/src/config/prod.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   DATABASE_URI: "mongodb://localhost:27017/airbnb-clone",
   JWT_SECRET: "Your-Session-Secret-Goes-Here",
+  SESSION_SECRET: "some-session-secret",
   PORT: 4000,
   SERVER_URL: "http://localhost:4000",
   FRONTEND_URL: "http://localhost:3000"
