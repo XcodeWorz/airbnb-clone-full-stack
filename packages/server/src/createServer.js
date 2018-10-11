d19ef92fab1f62d0bd597dc982b4a725095602fe
Laura <laurazenc@gmail.com>
2018-10-11 20:26:09 +0200

    Refactor code

--- packages/server/src/createServer.js
+++ packages/server/src/createServer.js
@@ -1,13 +1,21 @@
+import config from './config';
+
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
 const { GraphQLServer } = require('graphql-yoga');
 const { importSchema } = require('graphql-import');
+
+const redis = require('./redis');
+const { redisSessionPrefix } = require('./utils/constants');
 
 const resolvers = require('./resolvers');
 const connectDatabase = require('./db');
 
 const db = connectDatabase();
 
-function createServer(redis) {
-  return new GraphQLServer({
+const createServer = () => {
+  const server = new GraphQLServer({
     typeDefs: importSchema('src/schema.graphql'),
     resolverValidationOptions: {
       requireResolversForResolveType: false,
@@ -21,6 +29,44 @@ function createServer(redis) {
       db,
     }),
   });
-}
+
+  server.express.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+        prefix: redisSessionPrefix,
+      }),
+      name: 'qid',
+      secret: config.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      },
+    }),
+  );
+
+  server.express.use((req, res, next) => {
+    if (!req.session) {
+      return next(new Error('oh no')); // handle error
+    }
+    next(); // otherwise continue
+  });
+
+  server.start(
+    {
+      cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL || config.FRONTEND_URL,
+      },
+      port: process.env.PORT || config.PORT,
+    },
+    ({ port }) => {
+      console.log(`[⚙️ ] Server is up and running at http://localhost:${port}`);
+    },
+  );
+};
 
 module.exports = createServer;
