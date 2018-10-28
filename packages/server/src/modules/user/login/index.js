5cc3438028ca6a0e6bc7a8687b3b2a9869f293e2
Laura <laurazenc@gmail.com>
2018-10-28 21:17:53 +0100

    Use redis sessions

--- packages/server/src/modules/user/login/index.js
+++ packages/server/src/modules/user/login/index.js
@@ -1,30 +1,37 @@
-import jwt from 'jsonwebtoken';
-import { userMessages } from '@airbnb-clone/common';
-import { User } from '../../../models';
-import { handleErrors } from '../../../utils/handleErrors';
+import jwt from "jsonwebtoken";
+import { userMessages } from "@airbnb-clone/common";
+import { User } from "../../../models";
+import { handleErrors } from "../../../utils/handleErrors";
+import { userSessionIdPrefix } from "./../../../utils/constants";
 
-import config from '../../../config';
+import config from "../../../config";
 
-export const login = async (_, { email, password }) => {
+export const login = async (
+  _,
+  { email, password },
+  { session, redis, req }
+) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    return handleErrors('email', userMessages.invalidLogin);
+    return handleErrors("email", userMessages.invalidLogin);
   }
 
   const valid = await user.authenticateUser(password);
 
   if (!valid) {
-    return handleErrors('password', userMessages.invalidLogin);
+    return handleErrors("password", userMessages.invalidLogin);
   }
 
-  const token = jwt.sign({ _id: user._id, email: user.email }, config.JWT_SECRET);
+  const token = jwt.sign(
+    { _id: user._id, email: user.email },
+    config.JWT_SECRET
+  );
 
-  // session.userId = user.id;
-
-  // if (request.sessionID) {
-  //   await redis.lpush(`userSids:${user.id}`, request.sessionID);
-  // }
+  session.userId = user.id;
+  if (req.sessionID) {
+    await redis.lpush(`${userSessionIdPrefix}${user.id}`, req.sessionID);
+  }
 
   return { token };
 };
