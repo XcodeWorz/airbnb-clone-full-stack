46c0cac1123e08a2717f31f7ced7c15e949f4e6f
Laura <laurazenc@gmail.com>
2018-11-07 23:46:18 +0100

    Auth middleware

--- packages/server/src/modules/airbnb/create/index.js
+++ packages/server/src/modules/airbnb/create/index.js
@@ -1,6 +1,9 @@
 import Airbnb from '../../../models/Airbnb';
 
 export const createAirbnb = async (_, { data }, { session }) => {
+  if (!session.userId) {
+    throw new Error('not authenticated');
+  }
   await Airbnb.create({ ...data, host: session.userId });
   return true;
 };
