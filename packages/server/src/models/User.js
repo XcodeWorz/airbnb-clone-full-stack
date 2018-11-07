218e236e7f9fae9c6eda7e9d1359dc7fdf220763
Laura <laurazenc@gmail.com>
2018-11-07 19:24:18 +0100

    Add new account state for users

--- packages/server/src/models/User.js
+++ packages/server/src/models/User.js
@@ -1,41 +1,33 @@
-import mongoose, { Schema } from "mongoose";
-import { hashSync, compareSync } from "bcrypt-nodejs";
-import jwt from "jsonwebtoken";
-import config from "./../config";
+import mongoose from 'mongoose';
+import { hashSync, compareSync } from 'bcrypt-nodejs';
 
-const UserSchema = new Schema(
+const UserSchema = new mongoose.Schema(
   {
     email: { type: String, unique: true },
     firstName: String,
     lastName: String,
-    password: String
+    password: String,
+    accountLocked: { type: Boolean, default: false },
+    confirmed: { type: Boolean, default: false },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-UserSchema.pre("save", function(next) {
-  if (this.isModified("password")) {
-    this.password = this._hashPassword(this.password);
+UserSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    this.password = this.hashPassword(this.password);
     return next();
   }
   return next();
 });
 
 UserSchema.methods = {
-  _hashPassword(password) {
+  hashPassword(password) {
     return hashSync(password);
   },
   authenticateUser(password) {
     return compareSync(password, this.password);
   },
-  createToken() {
-    return jwt.sign(
-      {
-        _id: this._id
-      },
-      process.env.JWT_SECRET || config.JWT_SECRET
-    );
-  }
 };
 
-export default mongoose.model("User", UserSchema);
+export default mongoose.model('User', UserSchema);
