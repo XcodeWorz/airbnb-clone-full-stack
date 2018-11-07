274fff994216cefab3d48653f9befe2ec5936d7c
Laura <laurazenc@gmail.com>
2018-11-07 19:28:32 +0100

    Send emails logic

--- packages/server/src/utils/createForgotPasswordLink.js
+++ packages/server/src/utils/createForgotPasswordLink.js
@@ -3,6 +3,6 @@ import { forgotPasswordPrefix } from './constants';
 
 export const createForgotPasswordLink = async (url, userId, redis) => {
   const id = v4();
-  await redis.set(`${forgotPasswordPrefix}${id}`, userId, 'ex', 60 * 20);
+  await redis.set(`${forgotPasswordPrefix}${id}`, userId.toString(), 'ex', 60 * 20);
   return `${url}/change-password/${id}`;
 };
