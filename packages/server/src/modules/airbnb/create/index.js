a349cc532ddab4d43dd8d7d9af35ac5bbd0fef6a
Laura <laurazenc@gmail.com>
2018-11-08 16:54:12 +0100

    Validates airbnb schema

--- packages/server/src/modules/airbnb/create/index.js
+++ packages/server/src/modules/airbnb/create/index.js
@@ -1,6 +1,26 @@
+import { validAirbnbSchema } from '@airbnb-clone/common';
 import Airbnb from '../../../models/Airbnb';
 
+import { formatYupErrors } from '../../../utils/formatYupErrors';
+import { handleErrors } from '../../../utils/handleErrors';
+
 export const createAirbnb = async (_, { data }, { session }) => {
-  await Airbnb.create({ ...data, host: session.userId });
-  return true;
+  try {
+    const { latitude, longitude, ...rest } = data;
+    const newAirbnb = {
+      location: { type: 'Point', coordinates: [latitude, longitude] },
+      ...rest,
+      host: session.userId,
+    };
+
+    await validAirbnbSchema.validate(newAirbnb, { abortEarly: false });
+
+    await Airbnb.create(newAirbnb);
+    return {
+      result: true,
+    };
+  } catch (e) {
+    const { path, message } = formatYupErrors(e)[0];
+    return handleErrors(path, message);
+  }
 };
