d05b23e3a339fa8b0a691ba1c53900c48a625794
Laura <laurazenc@gmail.com>
2018-11-08 16:53:48 +0100

    Refactors me query

--- packages/client/src/pages/Home/index.js
+++ packages/client/src/pages/Home/index.js
@@ -1,33 +1,10 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
 
 import withFullPageLayout from "./../../layouts/FullPageLayout";
 
-const ME_QUERY = gql`
-  {
-    me {
-      result {
-        email
-      }
-      errors {
-        path
-        message
-      }
-    }
-  }
-`;
-
 class Home extends Component {
   render() {
-    return (
-      <Query query={ME_QUERY}>
-        {({ loading, error, data }) => {
-          console.log("me", data);
-          return <div>Home</div>;
-        }}
-      </Query>
-    );
+    return <div>Home</div>;
   }
 }
 
