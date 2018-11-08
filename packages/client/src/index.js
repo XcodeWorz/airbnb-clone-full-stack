2f4c47691bf00ee82c3c62a4e5ae8e4d5de10139
Laura <laurazenc@gmail.com>
2018-11-08 23:40:47 +0100

    Global theme variables

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -5,12 +5,37 @@ import { ApolloProvider } from "react-apollo";
 import * as serviceWorker from "./serviceWorker";
 import { client } from "./apollo";
 import { Routes } from "./routes";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 import "./style.less";
 
+const theme = {
+  primaryColor: "#FF5A5F",
+  linkColor: "#008489",
+  textColor: "#484848",
+  textColorSecondary: "#585A3A",
+  borderColorBase: "#E5E5E5",
+  boxShadowBase: "0 2px 4px rgba(0,0,0,0.1)"
+};
+
+const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`;
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <Routes />
+        <GlobalStyle />
+      </React.Fragment>
+    </ThemeProvider>
   </ApolloProvider>,
   document.getElementById("root")
 );
