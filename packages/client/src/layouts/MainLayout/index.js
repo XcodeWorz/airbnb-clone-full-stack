ced34394b97463bd4f27be1c7c78d3e8d5fb9a78
Laura <laurazenc@gmail.com>
2018-11-09 21:33:21 +0100

    Style Mainlayout

--- packages/client/src/layouts/MainLayout/index.js
+++ packages/client/src/layouts/MainLayout/index.js
@@ -5,6 +5,7 @@ import { Header } from "./components/Header";
 
 const MainLayoutWrapper = styled.div`
   height: 100vh;
+  width: 100%;
   position: relative;
   display: flex;
   align-items: center;
@@ -12,9 +13,10 @@ const MainLayoutWrapper = styled.div`
 `;
 
 const Content = styled.div`
-  display: flex;
+  position: relative;
   margin: 0 auto;
   width: 100%;
+  max-width: 960px;
 `;
 
 const withMainLayout = ChildComponent => {
