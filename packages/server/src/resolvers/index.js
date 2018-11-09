26ffad3bf17928da334ff24db5461d7f8bb64a71
Laura <laurazenc@gmail.com>
2018-11-09 21:36:02 +0100

    Handles special fields for Airbnbs

--- packages/server/src/resolvers/index.js
+++ packages/server/src/resolvers/index.js
@@ -1,7 +1,14 @@
-const { Query } = require('./Query');
-const { Mutation } = require('./Mutation');
+import User from "./../models/User";
+
+const { Query } = require("./Query");
+const { Mutation } = require("./Mutation");
 
 module.exports = {
+  Airbnb: {
+    image: (parent, _, { url }) =>
+      parent.image && `${url}/images/${parent.image}`,
+    host: async ({ host }) => User.findById(host)
+  },
   Query,
-  Mutation,
+  Mutation
 };
