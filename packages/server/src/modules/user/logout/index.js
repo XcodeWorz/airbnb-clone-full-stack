321269369fe9ce7d143d3369888b939da68054c8
Laura <laurazenc@gmail.com>
2018-11-10 14:36:34 +0100

    Logout resolver and clearing cookie from response

--- packages/server/src/modules/user/logout/index.js
+++ packages/server/src/modules/user/logout/index.js
@@ -1,7 +1,17 @@
-export const logout = async (_, __, { session }) => new Promise((res) => {
-  session.destroy((err) => {
-    if (err) throw new Error(err);
+import { removeUserSession } from '../../../utils/removeUserSession';
 
-    res(true);
-  });
-});
+export const logout = async (_, __, { session, redis, res }) => {
+  const { userId } = session;
+  if (userId) {
+    removeUserSession(userId, redis);
+    session.destroy((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    res.clearCookie('qid');
+    return true;
+  }
+
+  return false;
+};
---
+++ packages/server/src/utils/removeUserSession.js
@@ -0,0 +1,11 @@
+import { userSessionIdPrefix, redisSessionPrefix } from './constants';
+
+export const removeUserSession = async (userId, redis) => {
+  const sessionIds = await redis.lrange(`${userSessionIdPrefix}${userId}`, 0, -1);
+
+  const promises = [];
+  for (let i = 0; i < sessionIds.length; i += 1) {
+    promises.push(redis.del(`${redisSessionPrefix}${sessionIds[i]}`));
+  }
+  await Promise.all(promises);
+};
