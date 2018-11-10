515c383090bea9801d3a608ad577a6fda4edca66
Laura <laurazenc@gmail.com>
2018-11-10 14:21:07 +0100

    Applies userLoader to Airbnb resolver

--- packages/server/src/resolvers/index.js
+++ packages/server/src/resolvers/index.js
@@ -1,14 +1,11 @@
-import User from "./../models/User";
-
-const { Query } = require("./Query");
-const { Mutation } = require("./Mutation");
+const { Query } = require('./Query');
+const { Mutation } = require('./Mutation');
 
 module.exports = {
   Airbnb: {
-    image: (parent, _, { url }) =>
-      parent.image && `${url}/images/${parent.image}`,
-    host: async ({ host }) => User.findById(host)
+    image: (parent, _, { url }) => parent.image && `${url}/images/${parent.image}`,
+    host: ({ host }, _, { userLoader }) => userLoader.load(host),
   },
   Query,
-  Mutation
+  Mutation,
 };
